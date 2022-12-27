305688dfb5eba64f5e1b6c976152d92283b5e5a4
GeorgeKovshov <GeorgGreat@gmail.com>
2022-12-28 01:31:36 +0300

    made the viewer cycle through jpg's in folder, started working on zoom

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ import os
 root = Tk()
 root.title("Images")
 
-images=[]
 
+#Creating a list of image file names
+images=[]
 for f in os.listdir('.'):
     if f.endswith('.jpg'):
         images.append(f)
@@ -15,11 +16,15 @@ for f in os.listdir('.'):
 current_image = 0
 
 
-image = Image.open(images[0]).resize((400, 400))
+image = Image.open(images[current_image]).resize((400, 400))
 image1 = ImageTk.PhotoImage(image)
-actual_image = image1
+
 image1_label = Label(image=image1)
 image1_label.grid(row=0, column=0, columnspan=3)
+
+# storing current image in this variable, so garbage collector wouldn't get it
+actual_image = image1
+
 
 image2 = Image.open(images[1]).resize((400, 400))
 image22 = ImageTk.PhotoImage(image2)
@@ -27,33 +32,71 @@ image22 = ImageTk.PhotoImage(image2)
 
 
 #photoss = ImageTk.PhotoImage(images[0])
-
 #w = Canvas(root)
 #w.create_image(0, 0, 50, 50, image=photoss)
 
 
 def next_image(img_number):
+    """function that loads and puts the picture with 'img_number' from 'images' list on screen"""
     global images
     global current_image
     global image1_label
     global image22
     global actual_image
-    #current_image += 1
-    #label2 = Label(root, text=current_image)
-    #label2.grid(row=0, column=0)
+
+    # Calculating the next image number in list
+    current_image += (img_number - current_image)
+
+    # Showing the current image index
+    label2 = Label(root, text=current_image)
+    label2.grid(row=2, column=0)
 
 
-    #root.forget(image1_label)
+    # Replacing the old image with a new one
     image1_label.grid_forget()
-    image_new = Image.open(images[img_number-1]).resize((400, 400))
+    image_new = Image.open(images[current_image]).resize((400, 400))
     image1_new = ImageTk.PhotoImage(image_new)
     actual_image = image1_new
     image1_label = Label(image=actual_image)
 
-    button_next = Button(root, text="Next ->", command=lambda: next_image(img_number + 1))
-    image1_label.grid(row=0, column=0, columnspan=3)
-    button_next.grid(row=1, column=1)
 
+    # Making the image viewer cycle through images
+    next, previous = 0, 0
+    length_of_image_list = len(images)
+    if current_image + 2 > length_of_image_list:
+        previous = current_image - 1
+    elif current_image - 1 < 0:
+        next = current_image + 1
+        previous = length_of_image_list - 1
+    else:
+        next = current_image + 1
+        previous = current_image - 1
+
+    # Remapping the buttons to new images
+    button_next = Button(root, text="Next ->", command=lambda: next_image(next))
+    image1_label.grid(row=0, column=0, columnspan=3)
+    button_next.grid(row=1, column=2)
+    button_back = Button(root, text="<- Back", command=lambda: next_image(previous))
+    button_back.grid(row=1, column=0)
+
+
+def zoom(self):
+    global images
+    global current_image
+    global image1_label
+    global actual_image
+
+    zoom_value = zoom_slider.get()
+
+    # Try to put this into a button, maybe changing image while sliding is too taxing for tkinder
+    image1_label.grid_forget()
+    image_new = Image.open(images[current_image]).resize((600, 600))#resize((400 + (round(zoom_value)), (400 + round(zoom_value))))
+    image1_new = ImageTk.PhotoImage(image_new)
+    actual_image = image1_new
+    image1_label = Label(image=actual_image)
+
+    label = Label(root, text=zoom_value)
+    label.grid(row=3, column=2)
 
 
 
@@ -61,12 +104,24 @@ def next_image(img_number):
 #label.grid(row=2, column=0)
 
 
-
-button_next = Button(root, text="Next ->", command=lambda: next_image(2))
-button_next.grid(row=1, column=1)
+# Buttons when loading the program. If only one image in folder, the buttons are Disabled
+amount_of_images = len(images)
+if amount_of_images > 1:
+    button_next = Button(root, text="Next ->", command=lambda: next_image(1))
+    button_next.grid(row=1, column=2)
+    button_back = Button(root, text="<- Back", command=lambda: next_image(len(images) - 1))
+    button_back.grid(row=1, column=0)
+else:
+    button_next = Button(root, text="Next ->", command=DISABLED)
+    button_next.grid(row=1, column=2)
+    button_back = Button(root, text="<- Back", command=DISABLED)
+    button_back.grid(row=1, column=0)
 
 button_delete = Button(root, text="hide image")
-button_delete.grid(row=1, column=2)
+button_delete.grid(row=1, column=1)
+
+zoom_slider = Scale(root, from_=0, to=400, orient=HORIZONTAL, command=zoom)
+zoom_slider.grid(row=2, column=2)
 
 
 root.mainloop()
